1f078283257a2a511e4e9b39f1c54c060296ce20
Jan Carlos Blanco <41519591+janqui@users.noreply.github.com>
2023-04-13 23:53:35 +0000

    mpg

--- main.py
+++ main.py
@@ -3,3 +3,11 @@
 #read in the miles and read in the gallons used.
 
 #display the miles per gallon
+
+miles = float(input())
+
+gallons = float(input())
+
+mpg = float(miles / gallons)
+
+print(mpg)
